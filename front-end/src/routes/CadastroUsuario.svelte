4b9b1053d6ea0b30c59570fcc3b2738b35c9ade1
PCdaFacul <gustavo.michels.de.camargo@gmail.com>
2024-06-18 16:00:29 -0300

    Terminando a tela de cadastrar Usuario

--- front-end/src/routes/CadastroUsuario.svelte
+++ front-end/src/routes/CadastroUsuario.svelte
@@ -1,23 +1,134 @@
-<script></script>
+<script>
+    // Importe a variável reativa do Svelte
+    import Popup from "../lib/Popup.svelte";
 
-<form>
-    <div class="mb-3">
-        <label for="cadastroEmail" class="form-label">Email address</label>
-        <input
-            type="email"
-            class="form-control"
-            id="cadastroEmail"
-            aria-describedby="emailHelp"
+    // Crie variáveis reativas para os campos
+    let nome = "";
+    let email = "";
+    let senha = "";
+    let confirmarSenha = "";
+    let popupVisivel = false;
+    let popupMensagem = "";
+    let popupCallBack = null;
+
+    // Função para verificar se as senhas coincidem
+    function verificarCampos() {
+        if (senha !== confirmarSenha) {
+            abrirPopup("As senhas não coincidem. Por favor, tente novamente.");
+        }
+
+        if ([nome, email, senha, confirmarSenha].some((i) => i === "")) {
+            abrirPopup("Os campos não podem ficar em branco!");
+        }
+
+        if (validarEmail(email) === false) {
+            abrirPopup("O e-mail precisa ser valido!");
+        }
+    }
+
+    function abrirPopup(msg) {
+        popupMensagem = msg;
+        popupCallBack = () => {
+            popupVisivel = false;
+            popupMensagem = "";
+        };
+        popupVisivel = true;
+    }
+
+    function validarEmail(input) {
+        const validRegex =
+            /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+        if (input.match(validRegex)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+</script>
+
+<div class="container">
+    <picture>
+        <img
+            src="/imgs/Logo.jpeg"
+            class="img-fluid img-thumbnail logo"
+            alt="Imagem com formatos diferentes"
         />
-        <div id="emailHelp" class="form-text">
-            We'll never share your email with anyone else.
+    </picture>
+
+    <form>
+        <div class="row">
+            <h2>Cadastro de Usuario</h2>
         </div>
-    </div>
-    <div class="mb-3">
-        <label for="cadastroSenha" class="form-label">Password</label>
-        <input type="password" class="form-control" id="cadastroSenha" />
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+        <div class="mb-3">
+            <div class="row">
+                <label for="cadastroNome" class="form-label">Nome</label>
+            </div>
+            <div class="row">
+                <input
+                    type="text"
+                    class="form-control"
+                    id="cadastroNome"
+                    bind:value={nome}
+                />
+            </div>
+        </div>
+        <div class="mb-3">
+            <div class="row">
+                <label for="cadastroEmail" class="form-label">E-mail</label>
+            </div>
+            <div class="row">
+                <input
+                    type="email"
+                    class="form-control"
+                    id="cadastroEmail"
+                    aria-describedby="emailHelp"
+                    bind:value={email}
+                />
+            </div>
+        </div>
+        <div class="mb-3">
+            <div class="row">
+                <label for="cadastroSenha" class="form-label">Password</label>
+            </div>
+            <div class="row">
+                <input
+                    type="password"
+                    class="form-control"
+                    id="cadastroSenha"
+                    bind:value={senha}
+                />
+            </div>
+        </div>
+        <div class="mb-3">
+            <div class="row">
+                <label for="cadastroConfirmarSenha" class="form-label"
+                    >Confirmar Senha</label
+                >
+            </div>
+            <div class="row">
+                <input
+                    type="password"
+                    class="form-control"
+                    id="cadastroConfirmarSenha"
+                    bind:value={confirmarSenha}
+                />
+            </div>
+        </div>
+        <div class="row">
+            <button
+                type="button"
+                class="btn btn-warning botao-amarelo"
+                on:click={verificarCampos}>Registrar conta de usuario</button
+            >
+        </div>
+    </form>
+</div>
+<Popup
+    open={popupVisivel}
+    message={popupMensagem}
+    title={"Erro"}
+    onClosed={popupCallBack}
+/>
 
 <style></style>
