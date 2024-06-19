b63239e430a083f81cefdb17bd9b0745c5d5b58f
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-06-19 18:56:26 -0300

    Alterando string

--- back-end/src/db.rs
+++ back-end/src/db.rs
@@ -1,7 +1,7 @@
 use postgres::{Client, NoTls};
 
 pub fn get_client() -> Client {
-    match Client::connect("host=localhost port=5051 user=admin password=admin", NoTls) {
+    match Client::connect("postgresql://admin:admin@localhost:5051/odaw", NoTls) {
         Ok(valor) => valor,
         Err(err) => panic!("Deu treta: {}", err),
     }
