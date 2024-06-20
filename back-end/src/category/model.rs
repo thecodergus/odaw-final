d202a618f27436e51d0bf1539716b59846c2b934
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-06-19 21:37:25 -0300

    Definindo categoria

--- back-end/src/category/model.rs
+++ back-end/src/category/model.rs
@@ -1,15 +1,16 @@
 use postgres::{Client, Error, Transaction};
 use rocket::http::ext::IntoCollection;
+use uuid::Uuid;
 
 use crate::db::get_client;
 
 pub struct Category {
-    id: Option<String>,
+    id: Option<Uuid>,
     nome: String,
-    id_documento: Option<String>,
+    id_documento: Option<Uuid>,
 }
 
-pub fn get_category(id: String) -> Result<Category, Error> {
+pub fn find_category(id: Uuid) -> Result<Category, Error> {
     match get_client().query_one("SELECT id, nome FROM categoria WHERE id='$1'", &[&id]) {
         Ok(row) => Ok(Category {
             id: Some(id),
@@ -20,7 +21,7 @@ pub fn get_category(id: String) -> Result<Category, Error> {
     }
 }
 
-pub fn get_categories(id_documento: String) -> Result<Vec<Category>, Error> {
+pub fn find_categories(id_documento: Uuid) -> Result<Vec<Category>, Error> {
     match get_client().query(
         "SELECT id, nome FROM categoria WHERE id_documento='$1'",
         &[&id_documento],
@@ -30,7 +31,7 @@ pub fn get_categories(id_documento: String) -> Result<Vec<Category>, Error> {
             .map(|r| Category {
                 id: r.get(0),
                 nome: r.get(1),
-                id_documento: Some(id_documento.to_string()),
+                id_documento: Some(id_documento),
             })
             .collect()),
         Err(err) => Err(err),
