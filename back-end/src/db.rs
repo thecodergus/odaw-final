a696ed19b9ee099608467474fa7bada6bcb6d229
PCdaFacul <gustavo.michels.de.camargo@gmail.com>
2024-06-24 11:03:47 -0300

    Usando env para guardar dados do banco de dados

--- back-end/src/db.rs
+++ back-end/src/db.rs
@@ -1,7 +1,25 @@
 use postgres::{Client, NoTls};
 
+use dotenvy::dotenv;
+use std::env;
+
 pub fn get_client() -> Client {
-    match Client::connect("postgresql://postgres:udesc@0.0.0.0:5432/odaw", NoTls) {
+    dotenv().ok();
+
+    let user_db = env::var("POSTGRES_USER").expect("Env com usuario do postgres não encontrado");
+    let password_db =
+        env::var("POSTGRES_PASSWORD").expect("Env com senha do postgres não encontrado");
+    let host_db = env::var("POSTGRES_HOST").expect("Env com host do postgres não encontrado");
+    let port_db = env::var("POSTGRES_PORT").expect("Env com porta do postgres não encontrado");
+    let database_db =
+        env::var("POSTGRES_DATABASE").expect("Env com database do postgres não encontrado");
+
+    let database_url: String = format!(
+        "postgresql://{}:{}@{}:{}/{}",
+        user_db, password_db, host_db, port_db, database_db
+    );
+
+    match Client::connect(&database_url, NoTls) {
         Ok(valor) => valor,
         Err(err) => panic!("Deu treta: {}", err),
     }
