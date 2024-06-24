551368f1e6d172de08ffc384de5493ab9489a398
PCdaFacul <gustavo.michels.de.camargo@gmail.com>
2024-06-24 10:15:59 -0300

    Terminando de fazer os models de document

--- back-end/src/document/model.rs
+++ back-end/src/document/model.rs
@@ -5,6 +5,7 @@ use postgres::{Error, Row};
 use rust_decimal::Decimal;
 use uuid::Uuid;
 
+#[derive(Clone)]
 pub enum Type {
     CONTA,
     RECEITA,
@@ -27,6 +28,7 @@ impl Type {
     }
 }
 
+#[derive(Clone)]
 pub struct Document {
     id: Option<Uuid>,
     tipo_documento: Type,
@@ -50,8 +52,8 @@ pub fn insert_document(doc: &Document) -> Result<(), Error> {
 // Function to update a document in the database
 pub fn update_document(doc: &Document) -> Result<(), Error> {
     match get_client().execute(
-        &format!("UPDATE documento SET tipo_de_documento = '{}', valor = '{}', data = $1, descricao = $2, id_usuario = $3 WHERE id = $4", doc.tipo_documento.to_string(), doc.valor.to_string()),
-        &[&doc.data, &doc.descricao, &doc.id_usuario],
+        &format!("UPDATE documento SET tipo_de_documento = '{}', valor = '{}', data = $1, descricao = $2 WHERE id = $3", doc.tipo_documento.to_string(), doc.valor.to_string()),
+        &[&doc.data, &doc.descricao, &doc.id],
     ){
         Ok(_) => Ok(()),
         Err(err) => Err(err)
@@ -67,14 +69,14 @@ pub fn delete_document(id: &Uuid) -> Result<(), Error> {
 }
 
 pub fn find_document(id: &Uuid) -> Result<Document, Error> {
-    match get_client().query_one("SELECT id, tipo_de_documento, CAST(valor AS VARCHAR), data, descricao, id_usuario FROM documento WHERE id = $1" , &[id]){
+    match get_client().query_one("SELECT id, CAST(tipo_de_documento AS VARCHAR), CAST(valor AS VARCHAR), data, descricao, id_usuario FROM documento WHERE id = $1" , &[id]){
         Ok(ret) => Ok(row_to_doc(ret)),
         Err(err) => Err(err)
     }
 }
 
 pub fn find_by_user(id: &Uuid) -> Result<Vec<Document>, Error> {
-    match get_client().query("SELECT id, tipo_de_documento, CAST(valor AS VARCHAR), data, descricao, id_usuario FROM documento WHERE id_usuario = $1", &[id]) {
+    match get_client().query("SELECT id, CAST(tipo_de_documento AS VARCHAR), CAST(valor AS VARCHAR), data, descricao, id_usuario FROM documento WHERE id_usuario = $1", &[id]) {
         Ok(ret) => Ok(ret.into_iter().map(row_to_doc).collect()),
         Err(err) => Err(err)
     }
@@ -93,9 +95,10 @@ fn row_to_doc(ret: Row) -> Document {
 
 #[cfg(test)]
 mod tests {
-    use std::str::FromStr;
+    use rocket::futures::SinkExt;
 
     use super::*;
+    use std::str::FromStr;
 
     #[test]
     fn test_insert_document() {
@@ -105,7 +108,7 @@ mod tests {
             valor: Decimal::from(100),
             data: NaiveDate::from_ymd_opt(2022, 1, 1).expect("Invalid date"),
             descricao: "Test document".to_string(),
-            id_usuario: Uuid::from_str("99d9cc6f-a1f9-4f51-9b39-2a49e1a2323b").expect("não é uuid"),
+            id_usuario: Uuid::from_str("01fb7f56-0f79-457d-92d5-d5e41045b5f2").expect("não é uuid"),
         };
 
         match insert_document(&doc) {
@@ -114,38 +117,112 @@ mod tests {
         }
     }
 
-    // #[test]
-    // fn test_update_document() {
-    //     let doc = Document {
-    //         id: None,
-    //         tipo_documento: Type::Revenue,
-    //         valor: Decimal::from(200),
-    //         data: NaiveDate::from_ymd_opt(2022, 2, 2).expect("Invalid date"),
-    //         descricao: "Updated document".to_string(),
-    //         id_usuario: "user456".to_string(),
-    //     };
+    #[test]
+    fn test_find_document_by_user() {
+        match find_by_user(
+            &Uuid::from_str("01fb7f56-0f79-457d-92d5-d5e41045b5f2").expect("não é uuid"),
+        ) {
+            Ok(_) => (),
+            Err(err) => panic!("Deu ruim: {}", err),
+        }
+    }
 
-    //     let result = update_document(&doc);
-    //     assert!(result.is_ok());
-    // }
+    #[test]
+    fn test_find_document() {
+        let doc = Document {
+            id: None,
+            tipo_documento: Type::RECEITA,
+            valor: Decimal::from(100),
+            data: NaiveDate::from_ymd_opt(2022, 1, 1).expect("Invalid date"),
+            descricao: "Test document".to_string(),
+            id_usuario: Uuid::from_str("01fb7f56-0f79-457d-92d5-d5e41045b5f2").expect("não é uuid"),
+        };
 
-    // #[test]
-    // fn test_delete_document() {
-    //     let doc = Document {
-    //         id: None,
-    //         tipo_documento: Type::Revenue,
-    //         valor: Decimal::from(200),
-    //         data: NaiveDate::from_ymd_opt(2022, 2, 2).expect("Invalid date"),
-    //         descricao: "Updated document".to_string(),
-    //         id_usuario: "user456".to_string(),
-    //     };
+        match insert_document(&doc) {
+            Ok(_) => (),
+            Err(err) => panic!("Deu ruim: {}", err),
+        }
 
-    //     let _ = insert_document(&doc);
-    //     // let doc_2 = find
+        let doc_2 = match find_by_user(
+            &Uuid::from_str("01fb7f56-0f79-457d-92d5-d5e41045b5f2").expect("não é uuid"),
+        ) {
+            Ok(v) => v.clone(),
+            Err(err) => panic!("Deu ruim: {}", err),
+        };
 
-    //     let id = "document_id".to_string();
+        for d in doc_2 {
+            match find_document(&d.id.unwrap()) {
+                Ok(_) => (),
+                Err(err) => panic!("Deu ruim: {}", err),
+            }
+        }
+    }
 
-    //     let result = delete_document(id);
-    //     assert!(result.is_ok());
-    // }
+    #[test]
+    fn test_update_document() {
+        let doc = Document {
+            id: None,
+            tipo_documento: Type::RECEITA,
+            valor: Decimal::from(100),
+            data: NaiveDate::from_ymd_opt(2022, 1, 1).expect("Invalid date"),
+            descricao: "Test document".to_string(),
+            id_usuario: Uuid::from_str("01fb7f56-0f79-457d-92d5-d5e41045b5f2").expect("não é uuid"),
+        };
+
+        match insert_document(&doc) {
+            Ok(_) => (),
+            Err(err) => panic!("Deu ruim: {}", err),
+        }
+
+        let doc_2 = match find_by_user(
+            &Uuid::from_str("01fb7f56-0f79-457d-92d5-d5e41045b5f2").expect("não é uuid"),
+        ) {
+            Ok(v) => v.clone(),
+            Err(err) => panic!("Deu ruim: {}", err),
+        };
+
+        match doc_2.first() {
+            Some(d) => {
+                let mut d2 = d.clone();
+                d2.descricao = "Descrição alterada".to_string();
+
+                match update_document(&d2) {
+                    Ok(_) => (),
+                    Err(err) => panic!("Deu ruim: {}", err),
+                }
+            }
+            None => panic!("Deu ruim"),
+        }
+    }
+
+    #[test]
+    fn test_delete_document() {
+        let doc = Document {
+            id: None,
+            tipo_documento: Type::RECEITA,
+            valor: Decimal::from(100),
+            data: NaiveDate::from_ymd_opt(2022, 1, 1).expect("Invalid date"),
+            descricao: "Test document".to_string(),
+            id_usuario: Uuid::from_str("01fb7f56-0f79-457d-92d5-d5e41045b5f2").expect("não é uuid"),
+        };
+
+        match insert_document(&doc) {
+            Ok(_) => (),
+            Err(err) => panic!("Deu ruim: {}", err),
+        }
+
+        let doc_2 = match find_by_user(
+            &Uuid::from_str("01fb7f56-0f79-457d-92d5-d5e41045b5f2").expect("não é uuid"),
+        ) {
+            Ok(v) => v.clone(),
+            Err(err) => panic!("Deu ruim: {}", err),
+        };
+
+        match doc_2.first() {
+            Some(d) => {
+                let _ = delete_document(&d.id.unwrap());
+            }
+            None => panic!("Nada encontrado"),
+        }
+    }
 }
